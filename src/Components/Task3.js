5896eca6d9a1bbf98f5fe0f476c62d0014f3688d
Divya Shree.R <drudregowdaostb2@gmail.com>
2019-12-05 17:14:21 +0530

    adding the task with four button without table

--- src/Components/Task3.js
+++ src/Components/Task3.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {updateA3,updateB3,updateC3,updateD3} from '../Actions/Task3Actions';
-import './Task3.css'; 
+import './Task3.css';
 
 class Task3 extends Component {
     render() {
         return (
-            <div>
-                 <table cellSpacing="20px" >
-                    <tr className="main_table">
-                        <td>{this.props.ageA}</td>
-                        <td>{this.props.ageB}</td>
-                        <td>{this.props.ageC}</td>
-                        <td>{this.props.ageD}</td>
-                    </tr>
-                    <tr className="main_table">
-                        <td className="button_in"><button onClick={()=>this.props.updateA3(this.props.ageC)}>UPDATE A</button></td>
-                        <td className="button_in"><button onClick={()=>this.props.updateB3(this.props.ageD)}>UPDATE B</button></td>
-                        <td className="button_in"><button onClick={()=>this.props.updateC3(this.props.ageA)}>UPDATE C</button></td>
-                        <td className="button_in"><button onClick={()=>this.props.updateD3(this.props.ageB)}>UPDATE D</button></td>
-                    </tr>
-                </table> 
-              
+            <div className="main">
+                
+                <div>{this.props.ageA}</div> 
+                <div> {this.props.ageB}</div> 
+                <div> {this.props.ageC}</div> 
+                <div> {this.props.ageD}</div>                               
+                <div><button className="button" onClick={()=>this.props.updateA3(this.props.ageC)}>UPDATE A</button></div>
+                <div><button className="button" onClick={()=>this.props.updateB3(this.props.ageD)}>UPDATE B</button></div>
+                <div><button className="button" onClick={()=>this.props.updateC3(this.props.ageA)}>UPDATE C</button></div>
+                <div> <button className="button" onClick={()=>this.props.updateD3(this.props.ageB)}>UPDATE D</button></div>
                 
            </div>
         );
